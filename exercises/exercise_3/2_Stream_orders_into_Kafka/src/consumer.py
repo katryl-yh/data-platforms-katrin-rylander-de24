f715ca516c5f807cdcd146210b4eb74b0fb9f4f0
Katrin katryl-yh <katrin@rylander.eu>
2025-01-28 14:59:00 +0100

    Ex 3 Uppgift 2

--- exercises/exercise_3/2_Stream_orders_into_Kafka/src/consumer.py
+++ exercises/exercise_3/2_Stream_orders_into_Kafka/src/consumer.py
@@ -1,45 +1,38 @@
 from quixstreams import Application
 
+# Initialize the Appication
 app = Application(
     broker_address="localhost:9092", 
     consumer_group="text-splitter", 
     auto_offset_reset="earliest"
     )
 
-jokes_topic = app.topic(name="jokes",
+# Define an input topic with JSON deserialization
+orders_topic = app.topic(name="orders",
                         value_deserializer="json"
                         )
-# sdf = streaming data frame
-# lazy evaluation (saves memory) vs eager evaluation
-# we save transformations, and we run them all on data when we need it
-sdf = app.dataframe(topic=jokes_topic)
-# we do not want to apply transformations to big amount of data
-# better strategy: mockup data, have a sample data, test your transformations and once you know what had to be don
-# then we scale up
-# we do not test on actual whole streamed data
+# sdf = StreamingDataFrame 
+# sdf is a lazy object (saves memory) 
+# It doesn't store the actual consumed data in memory, 
+# but only declares how the data should be transformed.
 
-
-# lambda function is anonymous function
-
-# def print_message(message):
-#   print(message)
-# sdf.update(print_message)
-# OR the same but in a shorter way
-sdf.update(lambda message: print(f"Input message: {message}"))
+# Create StreamingDataFrame and connect it to the topic 
+sdf = app.dataframe(topic=orders_topic)
 
 # we apply a function, we write a function that transforms the data
-# transformation: I want to split the words
+# transformation: I want to split the order by product, price and total price
 
-def split_words(message):
-    return [ {"word" : word} for word in message["joke_text"].split() ]
+def print_info(message):
+    print(f"\nInput: {message} \n")
+    total_price = 0
+    for element in message["products"]:
+        total_price +=element["quantity"]*element["price"]
+        print(f"Product: {element["name"]}\t\tQuantity: {element["quantity"]}\tPrice: {element["price"]}" )
+        
+    print(f"Total price: {total_price}\n")
 
-sdf = sdf.apply(split_words, expand=True)
+sdf.update(lambda message: print_info(message))
 
-sdf.update(lambda words: print(f"output: {words}"))
-
-sdf["length"] = sdf["word"].apply(lambda word: len(word))
-
-sdf = sdf.update(lambda row: print(f"Output: {row}"))
 
 if __name__ == "__main__":
     app.run()
--- exercises/exercise_3/2_Stream_orders_into_Kafka/src/producer.py
+++ exercises/exercise_3/2_Stream_orders_into_Kafka/src/producer.py
@@ -4,43 +4,48 @@ from  quixstreams import Application
 from pprint import pprint
 
 data_path = Path(__file__).parents[1] / "data"
-print()
-print(data_path)
-print()
+#print(f"\n{data_path= }\n")
 
-# reads in jokes, through its absolute path
-with open(data_path/ "jokes.json", "r", encoding="utf-8") as file:
-    jokes = json.load(file)
+# read in orders, use the absolute path to the file
+with open(data_path/ "orders.json", "r", encoding="utf-8") as file:
+    orders = json.load(file)
 
-#pprint(jokes)
-print()
-# consumer group allows parallel processing
-# a form of entry point for interacting with kafka
-# localhost:9092 is port mapped to broker container
+#print(f"Type of orders: {type(orders)}")
+
+# localhost:9092 is a port mapped to broker container
 # it only works when broker is up and running
 # server must be started locally
-app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
 
-# topic with jokes in json format, we need to change format of data into binary
-jokes_topic = app.topic(name="jokes", value_serializer="json")
+# step 1. Create an Application
+app = Application(
+    broker_address="localhost:9092", # Kafka broker address
+    consumer_group="text-splitter" # Kafka consumer group
+    )
 
-#print(jokes_topic)
+# value_serializer specifies how the value (i.e., the message content) 
+# should be serialized before sending it to the topic.
+# In this case, 'json' indicates that the message's value should be serialized into JSON format
+# before being sent. 
+# This ensures that the message can be properly interpreted by the consumer, 
+# as they will also expect the data to be in JSON format.
 
+# Step 2. Define a Topic and serialization
+order_topic = app.topic(name="orders", value_serializer="json")
+
+# Step 3. Create a Producer and produce messages
 def main():
     with app.get_producer() as producer:
-        # print(producer)
 
-        for joke in jokes: # we loop though a list of dictionaries
-            kafka_msg = jokes_topic.serialize(key=joke["joke_id"], value=joke)
-            print(f"\nProduced message: key = {kafka_msg.key},value = {kafka_msg.value}")
+        for order in orders: # we loop though a list of dictionaries
+            kafka_msg = order_topic.serialize(key=order["order_id"], value=order)
+            print(f"\n\nProduced message: \nkey = {kafka_msg.key},\nvalue = {kafka_msg.value}")
 
             producer.produce(
-                topic="jokes", 
+                topic="orders", 
                 key= str(kafka_msg.key), 
                 value= kafka_msg.value
             )
 
 # run this code only when executing this script not when importing this module
 if __name__ == '__main__':
-    # pprint(jokes)
     main()
